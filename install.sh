4559bd02831542396adae867a3451d73412c2705
Moritz Gunz <moritz.gunz@gmail.com>
2021-10-07 16:01:19 +0200

    chore: Add codespaces install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+export USER_SHELL="zsh"
+
+for i in .aliases .gitconfig .zshrc; do
+	ln --verbose --force --symbolic $PWD/$i ~/
+done
+echo "Creating symlinks done!"
+
+if [[ -z "$CLOUDSMITH_API_KEY" ]]; then
+	echo "CLOUDSMITH_API_KEY not set, not configuring credential helper"
+else
+	echo "Setting up git credential helper for CloudSmith"
+	
+	git config --global credential.helper store
+	echo "https://${CLOUDSMITH_API_USER}:${CLOUDSMITH_API_KEY}@dl.cloudsmith.io" >> ~/.git-credentials
+fi
+echo "Credential config done!"
